e22c931e4b980c6060d096f4d7bf71d66c5fe90d
Christian <chrizx0@gmail.com>
2016-07-06 17:14:42 -0400

    added a few extras in css,html to shorten javascript

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 
-
+  //version 2.05
+    $('button').click(function() {
+        $(this).toggleClass('on off');
+        $('body').toggleClass('light dark');
+        $('.status').toggle();
+    });
